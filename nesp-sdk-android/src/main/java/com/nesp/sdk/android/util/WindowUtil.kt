2bde00ceba66dc166b7229d0762d34d53770afe3
JinZhaoLu <1756404649@qq.com>
2020-12-03 21:49:41 +0800

    add icons

--- nesp-sdk-android/src/main/java/com/nesp/sdk/android/util/WindowUtil.kt
+++ nesp-sdk-android/src/main/java/com/nesp/sdk/android/util/WindowUtil.kt
@@ -1,6 +1,8 @@
 package com.nesp.sdk.android.util
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
 
 /**
  *
@@ -8,7 +10,7 @@ import android.content.Context
  * Team: NESP Technology
  * @author: <a href="mailto:1756404649@qq.com">靳兆鲁 Email:1756404649@qq.com</a>
  * Time: Created 2020/10/11 10:11
- Project: NespAndroidSdk
+Project: NespAndroidSdk
  **/
 class WindowUtil {
     companion object {
@@ -18,6 +20,16 @@ class WindowUtil {
             val resources = context.applicationContext.resources
             val identifier = resources.getIdentifier("status_bar_height", "dimen", "android")
             return resources.getDimensionPixelSize(identifier)
+        }
+
+        @JvmStatic
+        fun isSoftShowing(activity: Activity): Boolean {
+            //获取当前屏幕内容的高度
+            val screenHeight = activity.window.decorView.height
+            //获取View可见区域的bottom
+            val rect = Rect()
+            activity.window.decorView.getWindowVisibleDisplayFrame(rect)
+            return screenHeight - rect.bottom != 0
         }
     }
 }
