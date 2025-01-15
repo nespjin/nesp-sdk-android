c954aea598b7c997e39309f9c009bbc22dd5ace9
Jack Jin <1756404649@qq.com>
2025-01-15 17:55:22 +0800

    refactor(nesp-sdk-android): handle other lifecycle events in Navigator
    - Add an else branch to handle other lifecycle events in onStateChanged method
    - This change prepares for integrating lifecycle-aware components in the future

--- nesp-sdk-android/src/main/java/com/nesp/sdk/android/navigation/Navigator.kt
+++ nesp-sdk-android/src/main/java/com/nesp/sdk/android/navigation/Navigator.kt
@@ -195,6 +195,7 @@ class Navigator : LifecycleEventObserver {
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
         when (event) {
             Lifecycle.Event.ON_DESTROY -> onDestroy()
+            else -> {}
         }
     }
 
